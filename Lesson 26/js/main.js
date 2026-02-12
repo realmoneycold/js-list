585af85f81507f70b607b6fe42ea7e290593fc6d
moneycold <johanforever77@gmail.com>
2026-02-12 00:40:57 -1200

    Add files via upload

---
+++ Lesson 26/js/main.js
@@ -0,0 +1,74 @@
+// Ifoda 1
+// let A = 2
+// let B = 3
+// let Y = (-(1/(A**2)) * ((A**2) + (B**2)))
+// console.log(Y)
+// Ifoda 2
+// let A = 2
+// let B = 3
+// let C = 4
+// let D = 5
+// let Y = (((A + B)/(C + D))**2 + B**2)
+// console.log(Y)
+
+// Ifoda 3
+// let A = 2
+// let D = 3
+// let C = 4
+// let Y = 2*(A**D + ((4*C)**2)/3)
+// console.log(Y)
+
+// Ifoda 4
+// let A = 2
+// let B = 3
+// let C = 2
+// let D = 4
+// let Y = (1/(A**2) * ((B/10)**3) * ((C + D)*2))
+// console.log(Y)
+
+// Ifoda 5
+// let A = 2
+// let B = 3
+// let C = 4
+// let D = 2
+// let Y = (((A/B)-1)/(C/(D-1)))**2
+// console.log(Y)
+
+// Ifoda 6
+// let A = 2
+// let B = 3
+// let C = 4
+// let D = 2
+// let Y = (A + B) / (C + (D / (A + B)))
+// console.log(Y)
+
+// Ifoda 7
+// let A = 2
+// let B = 3
+// let C = 4
+// let T = ((A+B+C)**2-(A - B - C)**2)*(1/2)
+// let Y = Math.sqrt(T);
+// console.log(Y)
+
+// Ifoda 8
+// let A = 2
+// let B = 2
+// let C = 2
+// let Y = ((A**2)+(B**2)+(C**2))/(A*B*C)
+// console.log(Y)
+
+// Ifoda 9
+// let A = 2
+// let D = 3
+// let B = 4
+// let C = 2
+// let Y = A * ((D**3)/3 + B * ((C**2)/2))
+// console.log(Y)
+
+// Ifoda 10
+// let A = 2
+// let B = 3
+// let C = 3
+// let Y = (A/B - B/C) + (A+C)*1/4
+// console.log(Y)
+
